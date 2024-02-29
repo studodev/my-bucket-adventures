810678eab3a4c2c680b21b7dbc3681f6eb15260a
Robin Delbaere <hello@rdelbaere.fr>
2024-02-29 11:05:54 +0100

    Add middleware to routes

--- start/routes.ts
+++ start/routes.ts
@@ -2,24 +2,25 @@ import BucketsController from "#controllers/buckets_controller";
 import ItemsController from "#controllers/items_controller";
 import MainController from "#controllers/main_controller";
 import SecurityController from "#controllers/security_controller";
+import { middleware } from "#start/kernel";
 import router from '@adonisjs/core/services/router'
 
 // Main
 router.get('/', [MainController, 'home']).as('main_home')
 
 // Security
-router.get('/login', [SecurityController, 'form']).as('security_form')
-router.post('/login', [SecurityController, 'handle']).as('security_handle')
-router.get('/logout', [SecurityController, 'logout']).as('security_logout')
+router.get('/login', [SecurityController, 'form']).as('security_form').use(middleware.guest())
+router.post('/login', [SecurityController, 'handle']).as('security_handle').use(middleware.guest())
+router.get('/logout', [SecurityController, 'logout']).as('security_logout').use(middleware.auth())
 
 // Buckets
-router.get('/buckets', [BucketsController, 'list']).as('bucket_list')
-router.get('/buckets/new', [BucketsController, 'new']).as('bucket_new')
-router.post('/buckets/new', [BucketsController, 'store']).as('bucket_store')
-router.get('/buckets/:id', [BucketsController, 'view']).as('bucket_view')
-router.post('/buckets/:id/delete', [BucketsController, 'delete']).as('bucket_delete')
-router.post('/buckets/:id/item', [BucketsController, 'itemNew']).as('bucket_item_new')
+router.get('/buckets', [BucketsController, 'list']).as('bucket_list').use(middleware.auth())
+router.get('/buckets/new', [BucketsController, 'new']).as('bucket_new').use(middleware.auth())
+router.post('/buckets/new', [BucketsController, 'store']).as('bucket_store').use(middleware.auth())
+router.get('/buckets/:id', [BucketsController, 'view']).as('bucket_view').use(middleware.auth())
+router.post('/buckets/:id/delete', [BucketsController, 'delete']).as('bucket_delete').use(middleware.auth())
+router.post('/buckets/:id/item', [BucketsController, 'itemNew']).as('bucket_item_new').use(middleware.auth())
 
 // Item
-router.get('/items/:id/done', [ItemsController, 'done']).as('item_done')
-router.post('/items/:id/delete', [ItemsController, 'delete']).as('item_delete')
+router.get('/items/:id/done', [ItemsController, 'done']).as('item_done').use(middleware.auth())
+router.post('/items/:id/delete', [ItemsController, 'delete']).as('item_delete').use(middleware.auth())
